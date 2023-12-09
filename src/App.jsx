42693036cd65c47cba38c6ecc4de228cb0b25b46
Sairoden <odingandarosa@gmail.com>
2023-12-09 13:52:18 +0800

    Add react-hook-forms and react-hot-toast and implement Toaster

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 // REACT & LIBRARIES
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
 
 // PAGES
 import {
@@ -92,6 +93,22 @@ function App() {
     <>
       <RouterProvider router={router} />
       <Footer />
+      <Toaster
+        position="top-center"
+        gutter={12}
+        containerStyle={{ margin: "8px" }}
+        toastOptions={{
+          success: { duration: 3000 },
+          error: { duration: 5000 },
+          style: {
+            fontSize: "16px",
+            maxWidth: "500px",
+            padding: "16px",
+            backgroundColor: "var(--color-grey-0)",
+            color: "var(--color-grey-700)",
+          },
+        }}
+      />
     </>
   );
 }
