a51ef53e3bbf136e985cc4177105124e4f284f44
Dheyniel9 <dheynielnucum@gmail.com>
2023-12-14 02:40:33 +0800

    I improve the profile page

--- src/features/Account/Profile/Profile.jsx
+++ src/features/Account/Profile/Profile.jsx
@@ -76,261 +76,182 @@ function Profile() {
           </div>
         </div>
       </div>
-
       <div>
-        <div className="register_container">
-          <div className="register-main-container">
+        <div className="register_white_container">
+          <div className="register-gray-container">
             <div className="register_main_content">
               <div className="register-header">
                 <h4>REGISTERED LOST PET</h4>
                 <button className="register-btnAdd">ADD +</button>
               </div>
+              <div>
+                {" "}
+                <h3 className="register-footer-text">No Entry</h3>
+              </div>
             </div>
 
-            <div className="register-middle-content">
-              <motion.div
-                initial={{ opacity: 0, scale: 0.5 }}
-                animate={{ opacity: 1, scale: 1 }}
-                transition={{ duration: 0.7 }}
-                className="register-details"
-              >
-                <div className="register-container-img">
-                  <img src="shitsu.png" alt="cardImage" />
+            <div className="register-card-grid">
+              <div className="register-middle-container">
+                <div className="middle-img-container">
+                  <img
+                    src="/public/shitsu.png"
+                    alt="shitsupic"
+                    width={330}
+                    className="register-img"
+                  />
                 </div>
-
                 <div>
-                  <h3 className="register-middle-header">
-                    MO5-Poodle Tiny Yellow
-                  </h3>
-                  <span className="registered-spanText">
-                    GENDER: Female Last Seen: Nov 12,2019
-                  </span>
+                  <div>
+                    <h3>MO5-POODLE TINY YELLOW</h3>
+                  </div>
+                  <div className="register-span-container">
+                    <span className="span-text">Gender: male</span>
+                    <span className="span-text">Last seen: Nov 12, 2020</span>
+                  </div>
+                  <div className="register-button-container">
+                    <button className="update-btn">UPDATE</button>
+                    <button className="listing-btn">CLOSE LISTING</button>
+                  </div>
                 </div>
+              </div>
 
-                <div className="register-middle-btn">
-                  <button className="update-btn">UPDATE</button>
-                  <button className="listing-btn">CLOSE LISTING</button>
+              <div className="register-middle-container">
+                <div className="middle-img-container">
+                  <img
+                    src="/public/shitsu.png"
+                    alt="shitsupic"
+                    width={330}
+                    className="register-img"
+                  />
                 </div>
-              </motion.div>
-
-              <motion.div
-                initial={{ opacity: 0, scale: 0.5 }}
-                animate={{ opacity: 1, scale: 1 }}
-                transition={{ duration: 0.7 }}
-                className="register-details"
-              >
-                <div className="register-container-img">
-                  <img src="shitsu.png" alt="cardImage" />
-                </div>
-
                 <div>
-                  <h3 className="register-middle-header">
-                    MO5-Poodle Tiny Yellow
-                  </h3>
-                  <span className="registered-spanText">
-                    GENDER: Female Last Seen: Nov 12,2019
-                  </span>
+                  <div>
+                    <h3>MO5-POODLE TINY YELLOW</h3>
+                  </div>
+                  <div className="register-span-container">
+                    <span className="span-text">Gender: male</span>
+                    <span className="span-text">Last seen: Nov 12, 2020</span>
+                  </div>
+                  <div className="register-button-container">
+                    <button className="update-btn">UPDATE</button>
+                    <button className="listing-btn">CLOSE LISTING</button>
+                  </div>
                 </div>
+              </div>
 
-                <div className="register-middle-btn">
-                  <button className="update-btn">UPDATE</button>
-                  <button className="listing-btn">CLOSE LISTING</button>
+              <div className="register-middle-container">
+                <div className="middle-img-container">
+                  <img
+                    src="/public/shitsu.png"
+                    alt="shitsupic"
+                    width={330}
+                    className="register-img"
+                  />
                 </div>
-              </motion.div>
-
-              <motion.div
-                initial={{ opacity: 0, scale: 0.5 }}
-                animate={{ opacity: 1, scale: 1 }}
-                transition={{ duration: 0.7 }}
-                className="register-details"
-              >
-                <div className="register-container-img">
-                  <img src="shitsu.png" alt="cardImage" />
-                </div>
-
                 <div>
-                  <h3 className="register-middle-header">
-                    MO5-Poodle Tiny Yellow
-                  </h3>
-                  <span className="registered-spanText">
-                    GENDER: Female Last Seen: Nov 12,2019
-                  </span>
+                  <div>
+                    <h3>MO5-POODLE TINY YELLOW</h3>
+                  </div>
+                  <div className="register-span-container">
+                    <span className="span-text">Gender: male</span>
+                    <span className="span-text">Last seen: Nov 12, 2020</span>
+                  </div>
+                  <div className="register-button-container">
+                    <button className="update-btn">UPDATE</button>
+                    <button className="listing-btn">CLOSE LISTING</button>
+                  </div>
                 </div>
-
-                <div className="register-middle-btn">
-                  <button className="update-btn">UPDATE</button>
-                  <button className="listing-btn">CLOSE LISTING</button>
-                </div>
-              </motion.div>
-
-              <motion.div
-                initial={{ opacity: 0, scale: 0.5 }}
-                animate={{ opacity: 1, scale: 1 }}
-                transition={{ duration: 0.7 }}
-                className="register-details"
-              >
-                <div className="register-container-img">
-                  <img src="shitsu.png" alt="cardImage" />
-                </div>
-
-                <div>
-                  <h3 className="register-middle-header">
-                    MO5-Poodle Tiny Yellow
-                  </h3>
-                  <span className="registered-spanText">
-                    GENDER: Female Last Seen: Nov 12,2019
-                  </span>
-                </div>
-
-                <div className="register-middle-btn">
-                  <button className="update-btn">UPDATE</button>
-                  <button className="listing-btn">CLOSE LISTING</button>
-                </div>
-              </motion.div>
-
-              <motion.div
-                initial={{ opacity: 0, scale: 0.5 }}
-                animate={{ opacity: 1, scale: 1 }}
-                transition={{ duration: 0.7 }}
-                className="register-details"
-              >
-                <div className="register-container-img">
-                  <img src="shitsu.png" alt="cardImage" />
-                </div>
-
-                <div>
-                  <h3 className="register-middle-header">
-                    MO5-Poodle Tiny Yellow
-                  </h3>
-                  <span className="registered-spanText">
-                    GENDER: Female Last Seen: Nov 12,2019
-                  </span>
-                </div>
-
-                <div className="register-middle-btn">
-                  <button className="update-btn">UPDATE</button>
-                  <button className="listing-btn">CLOSE LISTING</button>
-                </div>
-              </motion.div>
-
-              <motion.div
-                initial={{ opacity: 0, scale: 0.5 }}
-                animate={{ opacity: 1, scale: 1 }}
-                transition={{ duration: 0.7 }}
-                className="register-details"
-              >
-                <div className="register-container-img">
-                  <img src="shitsu.png" alt="cardImage" />
-                </div>
-
-                <div>
-                  <h3 className="register-middle-header">
-                    MO5-Poodle Tiny Yellow
-                  </h3>
-                  <span className="registered-spanText">
-                    GENDER: Female Last Seen: Nov 12,2019
-                  </span>
-                </div>
-
-                <div className="register-middle-btn">
-                  <button className="update-btn">UPDATE</button>
-                  <button className="listing-btn">CLOSE LISTING</button>
-                </div>
-              </motion.div>
+              </div>
             </div>
-
             <div className="bottom-content">
               <Button variation="primary">SHOW MORE</Button>
             </div>
           </div>
 
-          <div className="register-footer-container">
-            <div className="register-footer-content">
+          <div className="register-gray-container">
+            <div className="register_main_content">
               <div className="register-header">
                 <h4>REGISTERED FOUND PET</h4>
                 <button className="register-btnAdd">ADD +</button>
               </div>
-
               <div>
+                {" "}
                 <h3 className="register-footer-text">No Entry</h3>
               </div>
             </div>
 
-            <div className="register-footer-image-container">
-              <motion.div
-                initial={{ opacity: 0, scale: 0.5 }}
-                animate={{ opacity: 1, scale: 1 }}
-                transition={{ duration: 0.7 }}
-                className="register-details"
-              >
-                <div className="register-container-img">
-                  <img src="shitsu.png" alt="cardImage" />
+            <div className="register-card-grid">
+              <div className="register-middle-container">
+                <div className="middle-img-container">
+                  <img
+                    src="/public/shitsu.png"
+                    alt="shitsupic"
+                    width={330}
+                    className="register-img"
+                  />
                 </div>
-
                 <div>
-                  <h3 className="register-middle-header">
-                    MO5-Poodle Tiny Yellow
-                  </h3>
-                  <span className="registered-spanText">
-                    GENDER: Female Last Seen: Nov 12,2019
-                  </span>
+                  <div>
+                    <h3>MO5-POODLE TINY YELLOW</h3>
+                  </div>
+                  <div className="register-span-container">
+                    <span className="span-text">Gender: male</span>
+                    <span className="span-text">Last seen: Nov 12, 2020</span>
+                  </div>
+                  <div className="register-button-container">
+                    <button className="update-btn">UPDATE</button>
+                    <button className="listing-btn">CLOSE LISTING</button>
+                  </div>
                 </div>
+              </div>
 
-                <div className="register-middle-btn">
-                  <button className="update-btn">UPDATE</button>
-                  <button className="listing-btn">CLOSE LISTING</button>
+              <div className="register-middle-container">
+                <div className="middle-img-container">
+                  <img
+                    src="/public/shitsu.png"
+                    alt="shitsupic"
+                    width={330}
+                    className="register-img"
+                  />
                 </div>
-              </motion.div>
-
-              <motion.div
-                initial={{ opacity: 0, scale: 0.5 }}
-                animate={{ opacity: 1, scale: 1 }}
-                transition={{ duration: 0.7 }}
-                className="register-details"
-              >
-                <div className="register-container-img">
-                  <img src="shitsu.png" alt="cardImage" />
-                </div>
-
                 <div>
-                  <h3 className="register-middle-header">
-                    MO5-Poodle Tiny Yellow
-                  </h3>
-                  <span className="registered-spanText">
-                    GENDER: Female Last Seen: Nov 12,2019
-                  </span>
+                  <div>
+                    <h3>MO5-POODLE TINY YELLOW</h3>
+                  </div>
+                  <div className="register-span-container">
+                    <span className="span-text">Gender: male</span>
+                    <span className="span-text">Last seen: Nov 12, 2020</span>
+                  </div>
+                  <div className="register-button-container">
+                    <button className="update-btn">UPDATE</button>
+                    <button className="listing-btn">CLOSE LISTING</button>
+                  </div>
                 </div>
+              </div>
 
-                <div className="register-middle-btn">
-                  <button className="update-btn">UPDATE</button>
-                  <button className="listing-btn">CLOSE LISTING</button>
+              <div className="register-middle-container">
+                <div className="middle-img-container">
+                  <img
+                    src="/public/shitsu.png"
+                    alt="shitsupic"
+                    width={330}
+                    className="register-img"
+                  />
                 </div>
-              </motion.div>
-
-              <motion.div
-                initial={{ opacity: 0, scale: 0.5 }}
-                animate={{ opacity: 1, scale: 1 }}
-                transition={{ duration: 0.7 }}
-                className="register-details"
-              >
-                <div className="register-container-img">
-                  <img src="shitsu.png" alt="cardImage" />
-                </div>
-
                 <div>
-                  <h3 className="register-middle-header">
-                    MO5-Poodle Tiny Yellow
-                  </h3>
-                  <span className="registered-spanText">
-                    GENDER: Female Last Seen: Nov 12,2019
-                  </span>
+                  <div>
+                    <h3>MO5-POODLE TINY YELLOW</h3>
+                  </div>
+                  <div className="register-span-container">
+                    <span className="span-text">Gender: male</span>
+                    <span className="span-text">Last seen: Nov 12, 2020</span>
+                  </div>
+                  <div className="register-button-container">
+                    <button className="update-btn">UPDATE</button>
+                    <button className="listing-btn">CLOSE LISTING</button>
+                  </div>
                 </div>
-
-                <div className="register-middle-btn">
-                  <button className="update-btn">UPDATE</button>
-                  <button className="listing-btn">CLOSE LISTING</button>
-                </div>
-              </motion.div>
+              </div>
             </div>
             <div className="bottom-content">
               <Button variation="primary">SHOW MORE</Button>
