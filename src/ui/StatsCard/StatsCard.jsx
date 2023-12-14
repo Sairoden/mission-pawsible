d7cabf18e31e9924e5c1082cc880c921ad155b81
LazyTurtle-chiu <76042466+LazyTurtle-chiu@users.noreply.github.com>
2023-12-15 00:42:21 +0800

    added stats card ui

---
+++ src/ui/StatsCard/StatsCard.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import "./StatsCard.scss"
+
+const StatsCard = ({
+    number,
+    text,
+    image
+    }) => {
+  return (
+    <div className="card">
+        <div className="stats">
+            <div className="header">
+                <h3>{number}</h3>
+            </div>
+            <div className="body">
+                <h6>{text}</h6>
+            </div>
+        </div>
+        <div className="img">
+            <img src={image} alt=""/>
+        </div>
+    </div>
+  )
+}
+
+export default StatsCard
