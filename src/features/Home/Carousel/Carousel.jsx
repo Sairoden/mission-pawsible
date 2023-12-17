110523c3d212439bc70553e43ebad4cabe33d2b8
LazyTurtle-chiu <76042466+LazyTurtle-chiu@users.noreply.github.com>
2023-12-18 00:02:43 +0800

    add carousel + scss

--- src/features/Home/Carousel/Carousel.jsx
+++ src/features/Home/Carousel/Carousel.jsx
@@ -2,7 +2,29 @@
 import "./Carousel.scss";
 
 function Carousel() {
-  return <h1>Carousel</h1>;
+  return (
+    <section className="carousel">
+    <div className="info">
+        <div className="header">
+            <h2>Recent Listing</h2>
+        </div>
+        <div className="body">
+            <h5>Help bring a paw back home! Start your search now, your furry friends can’t wait to see you!</h5>
+        </div>
+        <div className="btn">
+            <div className="button bg-orange">
+                <h5>Join Us</h5>
+            </div>
+        </div>
+    </div>
+    <div className="slider" id="slider">
+        <img src="./public/img-slider1.png" alt="" />
+        <img src="./public/img-slider2.png" alt="" />
+        <img src="./public/img-slider3.png" alt="" />
+        <img src="./public/img-slider1.png" alt="" />
+    </div>
+</section>
+); 
 }
 
 export default Carousel;
