1146dd751a12fca44b4c8df5058e1d4a9f9dd35b
Dheyniel9 <dheynielnucum@gmail.com>
2023-12-23 05:05:11 +0800

    changing stats section in homepage

--- src/features/Home/Stats/Stats.jsx
+++ src/features/Home/Stats/Stats.jsx
@@ -2,19 +2,46 @@
 import "./Stats.scss";
 
 // UI COMPONENTS
-import { StatsCard } from "../../../ui";
+import { Button } from "../../../ui";
 
 function Stats() {
   return (
-    <div>
-      <div className="img-paw"></div>
-      <section className="statistic">
-        <div className="container">
-          <StatsCard number="100" text="MISSING PETS" image="/img-paw.png" />
-          <StatsCard number="100" text="FOUND PETS" image="/img-paw.png" />
-          <StatsCard number="100" text="REUNITED PETS" image="/img-paw.png" />
+    <div className="stats-container">
+      <div className="stats-content">
+        <div className="stats-header-text">
+          <h1 className="stats-one">One</h1>
+          <h1 className="stats-many">Many</h1>
         </div>
-      </section>
+        <div>
+          <div className="stats-circle-card">
+            <div>
+              <div className="stats-circle1">
+                <h2 className="stats-circle-number">285</h2>
+                <br />
+                <span className="stats-circle-text">Missing Pets</span>
+              </div>
+            </div>
+            <div className="stats-circle2">
+              <h2 className="stats-circle-number">285</h2>
+              <br />
+              <span className="stats-circle-text">Missing Pets</span>
+            </div>
+            <div className="stats-circle3">
+              <h2 className="stats-circle-number">285</h2>
+              <br />
+              <span className="stats-circle-text">Missing Pets</span>
+            </div>
+          </div>
+          <hr className="stats-line" />
+        </div>
+        <div className="stats-footer">
+          <span>
+            Join the community of pet owners to reunite missing pets with their
+            families
+          </span>
+          <Button variation="primary">Sign up Now</Button>
+        </div>
+      </div>
     </div>
   );
 }
--- src/ui/StatsCard/StatsCard.jsx
+++ src/ui/StatsCard/StatsCard.jsx
@@ -3,19 +3,20 @@ import "./StatsCard.scss";
 
 function StatsCard({ number, text, image }) {
   return (
-    <div className="card">
-      <div className="stats">
-        <div className="header">
-          <h3>{number}</h3>
-        </div>
-        <div className="body">
-          <h6>{text}</h6>
-        </div>
-      </div>
-      <div className="img">
-        <img src={image} alt="" />
-      </div>
-    </div>
+    <div></div>
+    // <div className="card">
+    //   <div className="stats">
+    //     <div className="header">
+    //       <h3>{number}</h3>
+    //     </div>
+    //     <div className="body">
+    //       <h6>{text}</h6>
+    //     </div>
+    //   </div>
+    //   <div className="img">
+    //     <img src={image} alt="" />
+    //   </div>
+    // </div>
   );
 }
 
