3bad283a261a9b8ee6d05cd9a78a4e83f0aeebdb
Sairoden <odingandarosa@gmail.com>
2024-01-05 18:53:08 +0800

    Restyle navbar (NOT FINISHED)

--- src/ui/Navbar/Navbar.jsx
+++ src/ui/Navbar/Navbar.jsx
@@ -27,57 +27,71 @@ const Navbar = () => {
 
   return (
     <nav className="navbar-container">
-      <NavLink to="/">
-        <img src={logo} alt="mission pawsible logo" className="navbar-logo" />
-      </NavLink>
+      <input type="checkbox" className="toggler" />
+      <div className="hamburger">
+        <div className="hamburger-cross"></div>
+      </div>
 
-      <NavLink
-        to="/"
-        className={`nav-link ${({ isActive }) => (isActive ? "active" : "")}`}
-      >
-        <HiOutlineHomeModern className="navbar-icon" />
-        HOME
-      </NavLink>
+      <div className="menu">
+        <div className="header">
+          <NavLink to="/">
+            <img
+              src={logo}
+              alt="mission pawsible logo"
+              className="navbar-logo"
+            />
+          </NavLink>
 
-      <NavLink to="/lost-pets" className="nav-link">
-        <HiOutlineTag className="navbar-icon" />
-        LOST PETS
-      </NavLink>
+          <NavLink
+            to="/"
+            className={`nav-link ${({ isActive }) =>
+              isActive ? "active" : ""}`}
+          >
+            <HiOutlineHomeModern className="navbar-icon" />
+            HOME
+          </NavLink>
 
-      <NavLink to="/found-pets" className="nav-link">
-        <HiOutlineSignal className="navbar-icon" />
-        FOUND PETS
-      </NavLink>
+          <NavLink to="/lost-pets" className="nav-link">
+            <HiOutlineTag className="navbar-icon" />
+            LOST PETS
+          </NavLink>
 
-      <NavLink to="/reunited-pets" className="nav-link">
-        <HiOutlineTrophy className="navbar-icon" />
-        REUNITED PETS
-      </NavLink>
+          <NavLink to="/found-pets" className="nav-link">
+            <HiOutlineSignal className="navbar-icon" />
+            FOUND PETS
+          </NavLink>
 
-      <NavLink to="/resources" className="nav-link">
-        <HiOutlineSquare3Stack3D className="navbar-icon" />
-        RESOURCES
-      </NavLink>
+          <NavLink to="/reunited-pets" className="nav-link">
+            <HiOutlineTrophy className="navbar-icon" />
+            REUNITED PETS
+          </NavLink>
 
-      <NavLink to="/about-us" className="nav-link">
-        <HiOutlineLightBulb className="navbar-icon" />
-        ABOUT US
-      </NavLink>
+          <NavLink to="/resources" className="nav-link">
+            <HiOutlineSquare3Stack3D className="navbar-icon" />
+            RESOURCES
+          </NavLink>
 
-      <NavLink to="/contact-us" className="nav-link">
-        <HiOutlineMegaphone className="navbar-icon" />
-        CONTACT US
-      </NavLink>
+          <NavLink to="/about-us" className="nav-link">
+            <HiOutlineLightBulb className="navbar-icon" />
+            ABOUT US
+          </NavLink>
 
-      {!user ? (
-        <NavLink to="/login">
-          <Button variation="primary">LOG IN</Button>
-        </NavLink>
-      ) : (
-        <NavLink to="/account">
-          <Button variation="primary">ACCOUNT</Button>
-        </NavLink>
-      )}
+          <NavLink to="/contact-us" className="nav-link">
+            <HiOutlineMegaphone className="navbar-icon" />
+            CONTACT US
+          </NavLink>
+
+          {!user ? (
+            <NavLink to="/login">
+              <Button variation="primary">LOG IN</Button>
+            </NavLink>
+          ) : (
+            <NavLink to="/account">
+              <Button variation="primary">ACCOUNT</Button>
+            </NavLink>
+          )}
+        </div>
+      </div>
     </nav>
   );
 };
