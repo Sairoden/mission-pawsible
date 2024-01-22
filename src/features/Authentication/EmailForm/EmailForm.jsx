64d0e6ccda281874186abb180452d9e3b52672f3
LazyTurtle-chiu <76042466+LazyTurtle-chiu@users.noreply.github.com>
2024-01-22 16:01:00 +0800

    updated structure and css

--- src/features/Authentication/EmailForm/EmailForm.jsx
+++ src/features/Authentication/EmailForm/EmailForm.jsx
@@ -18,35 +18,57 @@ function EmailForm() {
   } = useForm();
 
   return (
-    <div className="email-form">
-      <img src={emailBanner} alt="Email banner" />
-
-      <h4>
-        Please provide the email address that you used when you signed up for
-        your account. We will send you an email that will allow you to reset
-        your password or verify your email again.
-      </h4>
-
-      <form action="">
-        <label htmlFor="email" className="login-label">
-          Email <ErrorInput>{errors?.email?.message}</ErrorInput>
-        </label>
-        <Input
-          id="email"
-          size="large"
-          type="email"
-          register={register}
-          required="Please provide a valid email address"
-        />
-
-        <Button variation="secondary" type="submit">
-          Resend email verifcation
-        </Button>
-
-        <Button variation="primary" icon={true} type="submit">
-          Forget password
-        </Button>
-      </form>
+    <div className="emailVer container spacing-t spacing-b margin-b">
+      {/*image container  */}
+      <div className="emailVer-img">
+        <img className="emailVer-img-content" src={emailBanner} alt="Email banner" />
+      </div>
+      {/* title container */}
+      <div className="title">
+        <h2>
+          Forgot your password?
+        </h2>
+      </div>
+      {/* body container */}
+      <div className="emailVer-body">
+        <p className="emailVer-body-text">
+          Please provide the email address that you used when you signed up for
+            your account. We will send you an email that will allow you to reset
+            your password or verify your email again.
+        </p>
+        <form className="emailVer-body-form" action="">
+          {/* email */}
+          <label htmlFor="email" className="label">
+            Email <ErrorInput>{errors?.email?.message}</ErrorInput>
+          </label>
+          <br />
+          <Input
+            id="email"
+            type="email"
+            register={register}
+            required="Please provide a valid email address"
+          />
+          <br />
+          {/* button */}
+          <div className="emailVer-body-form-button btns">
+            {/* resend email button */}
+            <Button variation="secondary"
+             size="width"
+             type="submit"
+            >
+              Resend email verifcation
+            </Button>
+            {/* forget password button */}
+            <Button variation="primary" 
+             size="width" icon={true} 
+             type="submit"
+            >
+              Forget password
+            </Button>
+          </div>
+          
+        </form>
+      </div>
     </div>
   );
 }
