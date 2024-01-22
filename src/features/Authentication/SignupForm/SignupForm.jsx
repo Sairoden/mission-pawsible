76bc19125dedb81bc6d1e1aed89cbd126b0f4f38
LazyTurtle-chiu <76042466+LazyTurtle-chiu@users.noreply.github.com>
2024-01-22 16:01:21 +0800

    updasted structure and css

--- src/features/Authentication/SignupForm/SignupForm.jsx
+++ src/features/Authentication/SignupForm/SignupForm.jsx
@@ -52,146 +52,181 @@ function SignupForm() {
   if (isPending || isPending2) return <Spinner />;
 
   return (
-    <div className="signup-container">
-      <motion.img
-        src={signupBanner}
-        alt="Guy with a dog image"
-        className="signup-banner"
-        initial={{ opacity: 0, scale: 0 }}
-        animate={{ opacity: 1, scale: 1 }}
-        transition={{ duration: 1 }}
-      />
-
-      <h1 className="signup-title">Be a part of our growing family today!</h1>
-
-      <form className="signup-form" onSubmit={handleSubmit(handleSignup)}>
-        <div className="grid-container">
-          <div className="grid-item">
-            <label htmlFor="firstName" className="signup-label">
-              First Name <ErrorInput>{errors?.firstName?.message}</ErrorInput>
-            </label>
-            <Input
-              id="firstName"
-              width="22vw"
-              register={register}
-              required="Please provide a first name"
-              disabled={isPending || isPending2}
-            />
+    <div className="signUp container spacing-t spacing-b margin-b">
+      {/* image container */}
+      <div className="signUp-img">
+        <motion.img
+          src={signupBanner}
+          alt="Guy with a dog image"
+          className="signUp-img-content"
+          initial={{ opacity: 0, scale: 0 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{ duration: 1 }}
+        />
+      </div>
+      {/* header container */}
+      <div className="signUp-header title">
+        <h2>Be a part of our growing family today!</h2>
+      </div>
+      {/* body contaienr */}
+      <div className="signUp-body">
+        {/* form */}
+        <form className="signUp-body-form" onSubmit={handleSubmit(handleSignup)}>
+          
+          {/* name container */}
+          <div className="signUp-body-form-name">
+            {/* last name */}
+            <div className="signUp-body-form-input">
+              <label htmlFor="lastName" className="label">
+                Last Name <ErrorInput>{errors?.lastName?.message}</ErrorInput>
+              </label>
+              <br />
+              <Input
+                id="lastName"
+                register={register}
+                required="Please provide a last name"
+                disabled={isPending || isPending2}
+              />
+              <br />
+            </div>
+            {/* first name */}
+            <div className="signUp-body-form-input">
+              <label htmlFor="firstName" className="label">
+                First Name <ErrorInput>{errors?.firstName?.message}</ErrorInput>
+              </label>
+              <br />
+              <Input
+                id="firstName"
+                register={register}
+                required="Please provide a first name"
+                disabled={isPending || isPending2}
+              />
+            </div>
+            <br />
           </div>
 
-          <div className="grid-item">
-            <label htmlFor="lastName" className="signup-label">
-              Last Name <ErrorInput>{errors?.lastName?.message}</ErrorInput>
-            </label>
-            <Input
-              id="lastName"
-              width="22vw"
-              register={register}
-              required="Please provide a last name"
-              disabled={isPending || isPending2}
-            />
-          </div>
-
-          <div className="grid-item">
-            <label htmlFor="email" className="signup-label">
-              Email <ErrorInput>{errors?.email?.message}</ErrorInput>
-            </label>
-            <Input
-              id="email"
-              width="22vw"
-              type="email"
-              register={register}
-              required="Please provide a valid email address"
-              disabled={isPending || isPending2}
-            />
-          </div>
-
-          <div className="grid-item">
-            <label htmlFor="contactNumber" className="signup-label">
-              Contact Number{" "}
-              <ErrorInput>{errors?.contactNumber?.message}</ErrorInput>
-            </label>
-            <Input
-              id="contactNumber"
-              width="22vw"
-              register={register}
-              required="Please provide"
-              disabled={isPending || isPending2}
-            />
-          </div>
-
-          <div className="grid-item">
-            <label htmlFor="address" className="signup-label">
+          {/* address */}
+          <div className="signUp-body-form-input">
+            <label htmlFor="address" className="label">
               Address <ErrorInput>{errors?.address?.message}</ErrorInput>
             </label>
+            <br />
             <Input
               id="address"
-              width="49vw"
               register={register}
               required="Please provide an address"
               disabled={isPending || isPending2}
             />
+            <br />
           </div>
 
-          <div className="grid-item">
-            <label htmlFor="password" className="signup-label">
-              Password <ErrorInput>{errors?.password?.message}</ErrorInput>
-            </label>
-            <Input
-              id="password"
-              width="22vw"
-              type="password"
-              register={register}
-              required="Please provide a password"
+          {/* contact container */}
+          <div className="signUp-body-form-contact">
+            {/* email */}
+            <div className="signUp-body-form-input">
+              <label htmlFor="email" className="label">
+                Email <ErrorInput>{errors?.email?.message}</ErrorInput>
+              </label>
+              <br />
+              <Input
+                id="email"
+                type="email"
+                register={register}
+                required="Please provide a valid email address"
+                disabled={isPending || isPending2}
+              />
+              <br />
+            </div>
+            {/* contact number */}
+            <div className="signUp-body-form-input">
+              <label htmlFor="contactNumber" className="label">
+                Contact Number{" "}
+                <ErrorInput>{errors?.contactNumber?.message}</ErrorInput>
+              </label>
+              <br />
+              <Input
+                id="contactNumber"
+                register={register}
+                required="Please provide"
+                disabled={isPending || isPending2}
+              />
+              <br />
+            </div>    
+          </div>
+
+          {/* password container */}
+          <div className="signUp-body-form-password">
+            {/* password */}
+            <div className="signUp-body-form-input">
+              <label htmlFor="password" className="label">
+                Password <ErrorInput>{errors?.password?.message}</ErrorInput>
+              </label>
+              <br />
+              <Input
+                id="password"
+                type="password"
+                register={register}
+                required="Please provide a password"
+                disabled={isPending || isPending2}
+              />
+              <br />
+            </div>
+            {/* confirm password */}
+            <div className="signUp-body-form-input">
+              <label htmlFor="confirmPassword" className="label">
+                Confirm Password{" "}
+                <ErrorInput>{errors?.confirmPassword?.message}</ErrorInput>
+              </label>
+              <br />
+              <Input
+                id="confirmPassword"
+                type="password"
+                register={register}
+                required="Password need to match"
+                password={getValues().password}
+                disabled={isPending || isPending2}
+              />
+            </div>
+            <br />
+          </div>
+          <br />
+          {/* signup button container */}
+          <div className="signUp-body-form-signup">
+            {/* login via google button */}
+            <button
+              type="button"
+              className="signUp-body-form-google-button button other button-width"
               disabled={isPending || isPending2}
-            />
-          </div>
-
-          <div className="grid-item">
-            <label htmlFor="confirmPassword" className="signup-label">
-              Confirm Password{" "}
-              <ErrorInput>{errors?.confirmPassword?.message}</ErrorInput>
-            </label>
-            <Input
-              id="confirmPassword"
-              width="22vw"
-              type="password"
-              register={register}
-              required="Password need to match"
-              password={getValues().password}
+              onClick={handleGoogleLogin}
+            >
+              Sign up with Google 
+              <FcGoogle className="signUp-body-form-google-logo" />
+            </button>
+            <br />
+            {/* sign up via user */}
+            <Button
+              variation="primary"
+              size="width"
+              icon={true}
+              type="submit"
               disabled={isPending || isPending2}
-            />
+            >
+              Signup
+            </Button>
           </div>
-        </div>
-
-        <div className="signup-buttons-container">
-          <button
-            type="button"
-            className="signup-google"
-            disabled={isPending || isPending2}
-            onClick={handleGoogleLogin}
-          >
-            Sign up with Google <FcGoogle className="signup-google-icon" />
-          </button>
-
-          <Button
-            variation="primary"
-            icon={true}
-            type="submit"
-            disabled={isPending || isPending2}
-            size="large"
-          >
-            Signup
-          </Button>
-        </div>
-      </form>
-
-      <Link to="/login" onClick={() => scrollTo(0, 0)}>
-        <p className="login-link">
+        </form>
+      </div>
+      <br />
+      {/* login link container */}
+      <div className="signUp-body-login">
+        {/* signup text */}
+        <h4 className="signUp-body-login-text">
           Already have an account?{" "}
-          <span className="login-link-bold">Log in</span>
-        </p>
-      </Link>
+          <Link to="/login" onClick={() => scrollTo(0, 0)}>
+            <span className="signUp-body-login-text-span">Log in</span>
+          </Link>
+        </h4>
+      </div>
     </div>
   );
 }
