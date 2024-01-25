740146c62332f4620cc0d9ae08cc0d3853762dc6
Dheyniel9 <dheynielnucum@gmail.com>
2024-01-25 14:48:18 +0800

    putting ribbon in registered pet

--- src/features/Account/RegisteredPetCard/RegisteredPetCard.jsx
+++ src/features/Account/RegisteredPetCard/RegisteredPetCard.jsx
@@ -1,25 +1,27 @@
 // STYLES
 import "./RegisteredPetCard.scss";
-
 import shitsu from "../../../assets/shitsu.png";
 
 function RegisteredPetCard() {
   return (
-    <div className="register-middle-container">
-      <div className="middle-img-container">
-        <img src={shitsu} alt="shitsupic" className="register-img" />
-      </div>
-      <div>
-        <div className="register-h3">
-          <h3 className="register-h3-text">MO5-POODLE TINY YELLOW</h3>
+    <div>
+      <div className="register-middle-container">
+        <div className="ribbon">Pending</div>
+        <div className="middle-img-container">
+          <img src={shitsu} alt="shitsupic" className="register-img" />
         </div>
-        <div className="register-span-container">
-          <span className="span-text">Gender: male</span>
-          <span className="span-text">Last seen: Nov 12, 2020</span>
-        </div>
-        <div className="register-button-container">
-          <button className="update-btn">UPDATE</button>
-          <button className="listing-btn">CLOSE LISTING</button>
+        <div>
+          <div className="register-h3">
+            <h3 className="register-h3-text">MO5-POODLE TINY YELLOW</h3>
+          </div>
+          <div className="register-span-container">
+            <span className="span-text">Gender: male</span>
+            <span className="span-text">Last seen: Nov 12, 2020</span>
+          </div>
+          <div className="register-button-container">
+            <button className="update-btn">UPDATE</button>
+            <button className="listing-btn">CLOSE LISTING</button>
+          </div>
         </div>
       </div>
     </div>
